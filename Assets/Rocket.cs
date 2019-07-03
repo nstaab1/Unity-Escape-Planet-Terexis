5b4dbad7e61b2586bb0ee47caf19e9fa34d3989b
Nathan Staab <nstaab443@gmail.com>
2019-07-02 23:56:25 -0400

    Added ship movement and ship audio

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+
+    Rigidbody rigidBody;
+    AudioSource rocketNoise;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
+        rocketNoise = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -21,16 +26,22 @@ public class Rocket : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.Space))
         {
-            print("Space");
+            if (!rocketNoise.isPlaying) {
+                rocketNoise.Play();
+            }
+            rigidBody.AddRelativeForce(Vector3.up);
+        } else
+        {
+            rocketNoise.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            print("Rotate Left");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("Rotate Right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
