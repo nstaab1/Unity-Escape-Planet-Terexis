d5fa4e14852c3b805e868714f312aa9b4aebeb83
Nathan Staab <nstaab443@gmail.com>
2019-07-04 22:06:25 -0400

    Completed Oscillation Script

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -9,8 +9,7 @@ public class Oscillator : MonoBehaviour
     [SerializeField] Vector3 movementVector = new Vector3(10f, 0f,0f);
     [SerializeField] float period = 2f;
 
-    // todo remove from inspector later
-    [Range(0,1)] [SerializeField] float movementFactor; // 0 not move 1 for fully moved
+    float movementFactor; // 0 not move 1 for fully moved
 
     Vector3 startingPos;
 
@@ -23,14 +22,16 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float cycles = Time.time / period; // grows continually from 0
-
-        const float tau = Mathf.PI * 2; // about 6.28
-        float rawSinWave = Mathf.Sin(cycles * tau);
-
-        print(rawSinWave);
-
+        movementFactor = CalculateMovementFactor();
         Vector3 offset = movementFactor * movementVector;
         transform.position = startingPos + offset;
+    }
+
+    private float CalculateMovementFactor()
+    {
+        float cycles = Time.time / period; // grows continually from 0
+        const float tau = Mathf.PI * 2; // about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau);
+        return rawSinWave / 2f + 0.5f;
     }
 }
